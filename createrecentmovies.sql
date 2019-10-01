9c08e681ae4d0fcfdab5019db3f829aa9a9a661b
Joel Pepera <peperaj@live.com>
2019-10-01 19:00:54 -0400

    Updating createrecentmovies.sql with comments

--- createrecentmovies.sql
+++ createrecentmovies.sql
@@ -1,4 +1,7 @@
-create table if not exists title_basics_movies as 
+-- script to filter moviesdb.table_basics table for only movies after 1999
+-- convert \N entries to NULL and convert some columns to numeric
+
+create temp table title_basics_movies as 
 	select * 
 	from title_basics
 	where titletype = 'movie';
@@ -10,10 +13,14 @@ update title_basics_movies
 update title_basics_movies
 	set endyear = NULL
 	where endyear = '\N';
-	
+
 update title_basics_movies
 	set runtimeminutes = NULL
 	where runtimeminutes = '\N';
+
+update title_basics_movies
+	set genres = NULL
+	where genres = '\N';
 
 alter table title_basics_movies
 	alter column startyear type integer using startyear::integer,
@@ -24,7 +31,3 @@ create table if not exists title_basics_movies_recent as
 	select * 
 	from title_basics_movies
 	where startyear > 1999;
-	
-update title_basics_movies_recent
-	set genres = NULL
-	where genres = '\N';
