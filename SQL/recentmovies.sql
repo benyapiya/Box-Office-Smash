bc9af1f96d5cccdb5bde30c2c4fdd105432a7cb9
Joel Pepera <peperaj@live.com>
2019-09-30 21:49:29 -0400

    Adding SQL script that creates recent movies table

---
+++ SQL/recentmovies.sql
@@ -0,0 +1,32 @@
+create table if not exists title_basics_movies as 
+	select * 
+	from title_basics
+	where titletype = 'movie';
+
+update title_basics_movies
+	set startyear = NULL
+	where startyear = '\N';
+
+update title_basics_movies
+	set endyear = NULL
+	where endyear = '\N';
+	
+update title_basics_movies
+	set runtimeminutes = NULL
+	where runtimeminutes = '\N';
+
+alter table title_basics_movies
+	alter column startyear type integer using startyear::integer,
+	alter column endyear type integer using endyear::integer,
+	alter column runtimeminutes type integer using runtimeminutes::integer;
+
+create table if not exists title_basics_movies_recent as
+	select * 
+	from title_basics_movies
+	where startyear > 1999;
+	
+update title_basics_movies_recent
+	set genres = NULL
+	where genres = '\N';
+
+select * from title_basics_movies_recent limit 500;
