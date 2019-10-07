3bec8cd9393eb942e48656da1f77f0021e12046f
Joel Pepera <peperaj@live.com>
2019-10-06 22:29:20 -0400

    Added principal names and lifetime_gross sales columns to movies_analytical_table.

--- SQL/create_movies_analytical_table.sql
+++ SQL/create_movies_analytical_table.sql
@@ -1,18 +1,58 @@
 --Script to create analytical table by parsing genre field into 3 fields
---Need to add code to attach top principals data to this analytical table...
+--Add principal names for each title
+
+create temp table analytical_table as
+select 	id,
+		t.tconst,
+		t.titletype,
+		t.primarytitle,
+		t.originaltitle,
+		t.region,
+		t.isadult,
+		t.startyear,
+		t.endyear,
+		t.runtimeminutes,
+		split_part(t.genres,',',1) as genre1,
+		split_part(t.genres,',',2) as genre2,
+		split_part(t.genres,',',3) as genre3,
+		r.nconst1,
+		r.name1,
+		r.category1,
+		r.nconst2,
+		r.name2,
+		r.category2,
+		r.nconst3,
+		r.name3,
+		r.category3,
+		r.nconst4,
+		r.name4,
+		r.category4,
+		r.nconst5,
+		r.name5,
+		r.category5,
+		r.nconst6,
+		r.name6,
+		r.category6,
+		r.nconst7,
+		r.name7,
+		r.category7,
+		r.nconst8,
+		r.name8,
+		r.category8
+from title_basics_movies_us_recent as t
+left join recent_us_principals_names as r on t.tconst=r.tconst;
 
 create table movies_analytical_table as
-select 	id,
-		tconst,
-		titletype,
-		primarytitle,
-		originaltitle,
-		region,
-		isadult,
-		startyear,
-		endyear,
-		runtimeminutes,
-		split_part(genres,',',1) as genre1,
-		split_part(genres,',',2) as genre2,
-		split_part(genres,',',3) as genre3
-from title_basics_movies_us_recent;
+select
+	a.*,
+	b.studio,
+	b.lifetime_gross
+from analytical_table as a
+left join box_office_mojo_title as b on a.primarytitle=b.title
+
+union select
+	a.*,
+	b.studio,
+	b.lifetime_gross
+from analytical_table as a
+left join box_office_mojo_title as b on a.originaltitle=b.title;
