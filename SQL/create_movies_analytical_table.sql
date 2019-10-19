c4fcd2c7859009760bc70b909c2338ee5deed14c
Joel Pepera <peperaj@live.com>
2019-10-19 13:44:40 -0400

    Removed union on original title

--- SQL/create_movies_analytical_table.sql
+++ SQL/create_movies_analytical_table.sql
@@ -1,8 +1,11 @@
 --Script to create analytical table by parsing genre field into 3 fields
 --Add principal names for each title
+--Add keywords and parse them into individual fields
 
+drop table analytical_table;
 create temp table analytical_table as
-select 	id,
+select distinct
+		id,
 		t.tconst,
 		t.titletype,
 		t.primarytitle,
@@ -88,49 +91,4 @@ select
 from analytical_table as a
 left join box_office_mojo_title as b on a.primarytitle=b.title
 left join title_rev_html as c on a.primarytitle=c.title
-left join title_dtls_html as d on a.primarytitle=d.title
-
-union select
-	a.*,
-	d.keywords,
-	split_part(d.keywords,',',1) as keyword1,
-	split_part(d.keywords,',',2) as keyword2,
-	split_part(d.keywords,',',3) as keyword3,
-	split_part(d.keywords,',',4) as keyword4,
-	split_part(d.keywords,',',5) as keyword5,
-	split_part(d.keywords,',',6) as keyword6,
-	split_part(d.keywords,',',7) as keyword7,
-	split_part(d.keywords,',',8) as keyword8,
-	split_part(d.keywords,',',9) as keyword9,
-	split_part(d.keywords,',',10) as keyword10,
-	split_part(d.keywords,',',11) as keyword11,
-	split_part(d.keywords,',',12) as keyword12,
-	split_part(d.keywords,',',13) as keyword13,
-	split_part(d.keywords,',',14) as keyword14,
-	split_part(d.keywords,',',15) as keyword15,
-	split_part(d.keywords,',',16) as keyword16,
-	split_part(d.keywords,',',17) as keyword17,
-	split_part(d.keywords,',',18) as keyword18,
-	split_part(d.keywords,',',19) as keyword19,
-	split_part(d.keywords,',',20) as keyword20,
-	split_part(d.keywords,',',21) as keyword21,
-	split_part(d.keywords,',',22) as keyword22,
-	split_part(d.keywords,',',23) as keyword23,
-	split_part(d.keywords,',',24) as keyword24,
-	split_part(d.keywords,',',25) as keyword25,
-	split_part(d.keywords,',',26) as keyword26,
-	split_part(d.keywords,',',27) as keyword27,
-	split_part(d.keywords,',',28) as keyword28,
-	split_part(d.keywords,',',29) as keyword29,
-	split_part(d.keywords,',',30) as keyword30,
-	split_part(d.keywords,',',30) as keyword31,
-	b.studio,
-	c.releasedate,
-	cast(replace(replace(b.lifetime_gross,'$',''),',','') as bigint) as lifetime_gross,
-	cast(replace(replace(c.productionbudget,'$',''),',','') as bigint) as productionbudget,
-	cast(replace(replace(c.domesticgross,'$',''),',','') as bigint) as domesticgross,
-	cast(replace(replace(c.worldwidegross,'$',''),',','') as bigint) as worldwidegross
-from analytical_table as a
-left join box_office_mojo_title as b on a.originaltitle=b.title
-left join title_rev_html as c on a.originaltitle=c.title
-left join title_dtls_html as d on a.originaltitle=d.title;
+left join title_dtls_html as d on a.primarytitle=d.title;
